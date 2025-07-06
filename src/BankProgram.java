a23ce19c1a9f2771265ef579a2c62d4b49238a4d
Nandan <167064786+Nandan206@users.noreply.github.com>
2025-07-06 22:29:56 +0530

    updated design of choice system

--- src/BankProgram.java
+++ src/BankProgram.java
@@ -8,14 +8,14 @@ public class BankProgram {
         int choice;
 
         while(isRunning){
-            System.out.println("***************");
+            System.out.println("---------------");
             System.out.println("Welcome to Bank");
-            System.out.println("***************");
+            System.out.println("---------------");
             System.out.println("1.Show Balance");
             System.out.println("2.Deposit");
             System.out.println("3.Withdraw");
             System.out.println("4.Exit");
-            System.out.println("***************");
+            System.out.println("---------------");
 
             System.out.print("Enter your choice: ");
             choice = sc.nextInt();
@@ -30,12 +30,12 @@ public class BankProgram {
         sc.close();
     }
     static void showBalance(double balance){
-        System.out.println("***************");
+        System.out.println("----------------");
         System.out.printf("₹%.2f\n", balance);
     }
     static double deposit(){
         double amount;
-        System.out.println("****************************");
+        System.out.println("----------------------------");
         System.out.print("Enter amount to be deposited: ");
         amount = sc.nextDouble();
 
